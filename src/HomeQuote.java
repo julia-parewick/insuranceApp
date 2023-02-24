d45972472d3844f5ab06a1b00bcd8b83a1c10542
MasonSeward1 <mason.seward49@ed.cna.nl.ca>
2023-02-24 14:54:56 -0330

    code cleanup for readability

--- src/HomeQuote.java
+++ src/HomeQuote.java
@@ -16,7 +16,7 @@ public class HomeQuote extends Quote
      * @param startDate        the start date
      * @param endDate          the end date
      * @param premiumBeforeTax the premium before tax
-     * @param home             the home
+     * @param selectedHome     the home
      */
     public HomeQuote(LocalDate startDate, LocalDate endDate, Double premiumBeforeTax, Home selectedHome)
     {
@@ -61,12 +61,13 @@ public class HomeQuote extends Quote
             default -> heatingType = 6;
         }
 
-        double valuefactor = value > 250000 ? 0.2 : 0;
+        double valueFactor = value > 250000 ? 0.2 : 0;
         double ageFactor = age > 25 ? 1.25 : 0;
-        double heatFactor = heatingType==1 ? 1.00 : heatingType==2 ? 1.00 : heatingType==3 ? 2.00 : heatingType==4 ? 1.25 : heatingType==5 ? 1.00 : null;
+        double heatFactor = heatingType==1 ? 1.00 : heatingType==2 ? 1.00 : heatingType==3 ? 2.00 :
+                heatingType==4 ? 1.25 : heatingType==5 ? 1.00 : null;
         double locationFactor = home.isUrban() ? 1.00 : 1.25;
 
-        double totalFactor =  valuefactor*ageFactor*heatFactor*locationFactor;
-        return premium*totalFactor;
+        double totalFactor =  valueFactor * ageFactor * heatFactor * locationFactor;
+        return premium * totalFactor;
     }
 }
