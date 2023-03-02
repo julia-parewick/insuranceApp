a4b6ae7478b65ec3cdb3443951493169ac81a8c7
Dan Lewis <Daniel.Lewis37@ed.cna.nl.ca>
2023-03-02 13:45:13 -0330

    Tester class update

--- src/Tester.java
+++ src/Tester.java
@@ -3,13 +3,13 @@ import java.time.LocalDate;
 /**
  * @Author Adam Elliott
  * @Date 2023-02-28
- * Descriptive class for an Tester object
+ * Descriptive class for a Tester object
  */
 
 public class Tester {
 
     public static void main(String[] args) {
-        Driver driver = new Driver("Adam", "Elliott", "123456789", LocalDate.of(1999, 12, 31));
+        Driver driver = new Driver("Adam", "Elliott", 17, 1, "13 Deadend Dr");
         Accidents accident = new Accidents(LocalDate.of(2021, 2, 22), driver);
         System.out.println("Driver's name: " + accident.getDriverAtFault().getFirstName() + " " + accident.getDriverAtFault().getLastName()); // print the driver's name
         System.out.println("Date of accident: " + accident.getDate());
@@ -21,6 +21,7 @@ public class Tester {
         accident = new Accidents(LocalDate.of(2021, 2, 24), driver);
         System.out.println("Driver's name: " + accident.getDriverAtFault().getFirstName() + " " + accident.getDriverAtFault().getLastName());
         System.out.println("Date of accident: " + accident.getDate());
-        System.out.println("Driver's name: " + driver.getAccidents().get(0).getDriverAtFault().getFirstName() + " " + driver.getAccidents().get(0).getDriverAtFault().getLastName()); // print the driver's name
-        System.out.println("Date of accident: " + driver.getAccidents().get(0).getDate());
+        System.out.println("Driver's name: " + driver.getFirstName() + " " + driver.getLastName()); // print the driver's name
+        System.out.println("Date of accident: " + accident.getDate());
     }
+}
